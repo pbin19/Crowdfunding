b8c27ccc9b668ed90e5625f79ef50186b9fcf460
Krishna Bindu Polisetty <133198870+pbin19@users.noreply.github.com>
2025-06-06 20:37:20 +0530

    Update Axios.jsx

--- frontend/src/components/Axios.jsx
+++ frontend/src/components/Axios.jsx
@@ -1,6 +1,4 @@
-import axios from 'axios';
-
-const baseUrl = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000/api';
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const AxiosInstance = axios.create({
     baseURL: baseUrl,
